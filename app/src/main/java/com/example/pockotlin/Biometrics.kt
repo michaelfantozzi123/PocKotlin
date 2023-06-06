899ebc807b949f6fc188dfb6e7c412a81753bd66
Darren Potts <darrenp@agilebridge.co.za>
2023-06-06 13:27:14 +0200

    Biometrics

--- app/src/main/java/com/example/pockotlin/Biometrics.kt
+++ app/src/main/java/com/example/pockotlin/Biometrics.kt
@@ -1,14 +1,30 @@
 package com.example.pockotlin
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricPrompt.PromptInfo
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.example.pockotlin.databinding.FragmentBiometricsBinding
+import com.example.pockotlin.databinding.FragmentHomeBinding
+import java.util.concurrent.Executor
 
 class Biometrics : Fragment() {
 
+    private var _binding: FragmentBiometricsBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: PromptInfo
     companion object {
         fun newInstance() = Biometrics()
     }
@@ -19,7 +35,35 @@ class Biometrics : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_biometrics, container, false)
+        _binding = FragmentBiometricsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        checkDeviceHasBiometrics(requireContext())
+
+        executor = ContextCompat.getMainExecutor(requireContext())
+        biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(requireContext(), "Auth Failed", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(requireContext(), "Auth Success", Toast.LENGTH_LONG).show()
+            }
+        })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Authenticate User")
+            .setSubtitle("Authenticate with biometrics")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        binding.biometricButton.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -27,5 +71,26 @@ class Biometrics : Fragment() {
         viewModel = ViewModelProvider(this).get(BiometricsViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    private fun checkDeviceHasBiometrics(context: Context) {
+        Log.d("APP_TAG", "In method")
+        val biometricManager = BiometricManager.from(context)
+        Log.d("APP_TAG", "After In method")
+
+        when(biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                Log.d("APP_TAG", "App can auth with biometrics")
+                binding.canAuthMsg.text = "App can auth with biometrics"
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                Log.d("APP_TAG", "No Hardware")
+                binding.canAuthMsg.text = "No Hardware"
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                Log.d("APP_TAG", "Not enrolled biometrics")
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/pockotlin/MainActivity.kt
+++ app/src/main/java/com/example/pockotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pockotlin
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -11,6 +12,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import com.example.pockotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,28 +22,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+            setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            binding.appBarMain.fab.setOnClickListener { view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+            }
+            val drawerLayout: DrawerLayout = binding.drawerLayout
+            val navView: NavigationView = binding.navView
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            appBarConfiguration = AppBarConfiguration(setOf(
+                    R.id.nav_home, R.id.chart, R.id.nfc, R.id.biometrics, R.id.qr_scanner, R.id.bonus), drawerLayout)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.chart, R.id.nfc, R.id.biometrics, R.id.qr_scanner, R.id.bonus), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
