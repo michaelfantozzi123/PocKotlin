3a17150c8165f710a8b0498384f88ad526610d4a
StefanMarais <stefan.marais@kerridgecs.com>
2023-06-06 16:16:49 +0200

    nfc part 2

--- app/src/main/java/com/example/pockotlin/nfc.kt
+++ app/src/main/java/com/example/pockotlin/nfc.kt
@@ -1,16 +1,14 @@
 package com.example.pockotlin
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
