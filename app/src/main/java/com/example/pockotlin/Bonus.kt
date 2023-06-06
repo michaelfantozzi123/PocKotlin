bb0461b22a025e1506e93971c01b10097d649f0f
StefanMarais <stefan.marais@kerridgecs.com>
2023-06-06 14:24:46 +0200

    Location

--- app/src/main/java/com/example/pockotlin/Bonus.kt
+++ app/src/main/java/com/example/pockotlin/Bonus.kt
@@ -1,13 +1,22 @@
 package com.example.pockotlin
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class Bonus : Fragment() {
+    private val LOCATION_REQUEST_CODE = 1000
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     companion object {
         fun newInstance() = Bonus()
@@ -28,4 +37,53 @@ class Bonus : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+            return
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                if (location != null) {
+                    val textView: TextView = view.findViewById(R.id.text_view)
+                    textView.text = "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission was granted
+                    // You can now get the location
+                } else {
+                    // Permission was denied
+                    // You should tell the user that the feature is unavailable because the features requires a permission that the user has denied.
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
 }
